4e88023ae7a51a7580ae5c0fe40c80547be9423c
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2023-12-17 00:28:51 -0500

    changed spaces in css, renamed classes and fixed css duplication

--- Hidiveclone/scripts/animes-info.js
+++ Hidiveclone/scripts/animes-info.js
@@ -1,6 +1,6 @@
 export const recentlyAddedAnimeInfos = [{
   id: 'A0000-0000-0001',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'BLADE OF THE IMMORTAL',
   image: 'css/animePosters/BOI_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; E6 IN 2 DAYS',
@@ -8,7 +8,7 @@ export const recentlyAddedAnimeInfos = [{
 },
 {
   id: 'A0000-0000-0002',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'SHIROBAKO',
   image: 'css/animePosters/SHB_MASTER_300x169.jpg',
   newEpTimes: '&#128344; E1 IN 23 DAYS',
@@ -16,7 +16,7 @@ export const recentlyAddedAnimeInfos = [{
 },
 {
   id: 'A0000-0000-0003',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: `KINO'S JOURNEY`,
   image: 'css/animePosters/KJ_Master_300x169.jpg',
   newEpTimes: '&#128344; E6 IN 2 DAYS',
@@ -24,7 +24,7 @@ export const recentlyAddedAnimeInfos = [{
 },
 {
   id: 'A0000-0000-0004',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'THE DEMON SWORD MASTER OF EXCLAIBUR ACADEMY',
   image: 'css/animePosters/DEA_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; E11 IN 4 DAYS',
@@ -32,7 +32,7 @@ export const recentlyAddedAnimeInfos = [{
 },
 {
   id: 'A0000-0000-0005',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'TO LOVERU DARKNESS 2',
   image: 'css/animePosters/TLD_02_MASTER_300x169.jpg',
   newEpTimes: '&#128344; E1 IN 4 DAYS',
@@ -40,7 +40,7 @@ export const recentlyAddedAnimeInfos = [{
 },
 {
   id: 'A0000-0001-0001',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'HIMOUTO! UMARU-CHAN',
   image: 'css/animePosters/HUC_MASTER_300x169.jpg',
   newEpTimes: '&#128344; E1 IN 23 DAYS',
@@ -48,7 +48,7 @@ export const recentlyAddedAnimeInfos = [{
 },
 {
   id: 'A0000-0001-0002',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'KIDS ON THE SLOPE',
   image: 'css/animePosters/KOS_MASTER_300x169.jpg',
   newEpTimes: '&#128344; E1 IN 23 DAYS',
@@ -56,7 +56,7 @@ export const recentlyAddedAnimeInfos = [{
 },
 {
   id: 'A0000-0001-0003',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'MY LOVE STORY!!',
   image: 'css/animePosters/MLS_MASTER_300x169.jpg',
   newEpTimes: '&#128344; E1 IN 4 DAYS',
@@ -64,7 +64,7 @@ export const recentlyAddedAnimeInfos = [{
 },
 {
   id: 'A0000-0001-0004',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'LOVE, CHUNIBYO & OTHER DELUSIONS! HEART THROB-OVA',
   image: 'css/animePosters/CDK_02_OVA_MASTER_300x169.jpg',
   newEpTimes: '&#128344; E1 IN 3 DAYS',
@@ -72,7 +72,7 @@ export const recentlyAddedAnimeInfos = [{
 },
 {
   id: 'A0000-0001-0005',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'BEYOND THE BOUNDARY-OVA',
   image: 'css/animePosters/BTB_OVA_MASTER_300x169.jpg',
   newEpTimes: '&#128344; E1 IN 2 DAYS',
@@ -82,7 +82,7 @@ export const recentlyAddedAnimeInfos = [{
 
 export const SimulcastAnimeInfos = [{
   id: 'B0000-0000-0001',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'THE VEXATIONS OF A SHUT-IN VAMPIRE PRINCESS',
   image: 'css/SimulcastPosters/VVP_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; TODAY',
@@ -90,7 +90,7 @@ export const SimulcastAnimeInfos = [{
 },
 {
   id: 'B0000-0000-0002',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'RAGNA CRIMSON',
   image: 'css/SimulcastPosters/RCR_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; TODAY',
@@ -98,7 +98,7 @@ export const SimulcastAnimeInfos = [{
 },
 {
   id: 'B0000-0000-0003',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'DARK GATHERING',
   image: 'css/SimulcastPosters/DKG_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; TOMORROW',
@@ -106,7 +106,7 @@ export const SimulcastAnimeInfos = [{
 },
 {
   id: 'B0000-0000-0004',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'THE DEMON SWORD MASTER OF EXCLAIBUR ACADEMY',
   image: 'css/SimulcastPosters/DEA_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; E11 IN 2 DAYS',
@@ -114,7 +114,7 @@ export const SimulcastAnimeInfos = [{
 },
 {
   id: 'B0000-0000-0005',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'HELCK',
   image: 'css/SimulcastPosters/HLK_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; E23 IN 3 DAYS',
@@ -122,7 +122,7 @@ export const SimulcastAnimeInfos = [{
 },
 {
   id: 'B0000-0001-0001',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'THE EMINENCE IN SHADOW',
   image: 'css/SimulcastPosters/EIS_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -130,7 +130,7 @@ export const SimulcastAnimeInfos = [{
 },
 {
   id: 'B0000-0001-0002',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'CARDFIGHT!! VANGAURD',
   image: 'css/SimulcastPosters/VWD_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -138,7 +138,7 @@ export const SimulcastAnimeInfos = [{
 },
 {
   id: 'B0000-0001-0003',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'SPY CLASSROOM',
   image: 'css/SimulcastPosters/SCL_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -146,7 +146,7 @@ export const SimulcastAnimeInfos = [{
 },
 {
   id: 'B0000-0001-0004',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'THE MOST HERETICAL LAST BOSS QUEEN',
   image: 'css/SimulcastPosters/HLB_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -154,7 +154,7 @@ export const SimulcastAnimeInfos = [{
 },
 {
   id: 'B0000-0001-0005',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'THE DREAMING BOY IS A REALIST',
   image: 'css/SimulcastPosters/DBR_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -164,7 +164,7 @@ export const SimulcastAnimeInfos = [{
 
 export const exclusivesAnimeInfos = [{
   id: 'C0000-0000-0001',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'DARK GATHERING',
   image: 'css/SimulcastPosters/DKG_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; TODAY',
@@ -172,7 +172,7 @@ export const exclusivesAnimeInfos = [{
 },
 {
   id: 'C0000-0000-0002',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'LOVE, CHUNIBYO & OTHER DELUSIONS!-OVA',
   image: 'css/ExclusivePosters/CDK_01_OVA_MASTER_300x169.jpg',
   newEpTimes: '&#128344; TODAY',
@@ -180,7 +180,7 @@ export const exclusivesAnimeInfos = [{
 },
 {
   id: 'C0000-0000-0003',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'LOVE, CHUNIBYO & OTHER DELUSIONS! HEART THROB-OVA',
   image: 'css/ExclusivePosters/CDK_02_OVA_MASTER_300x169.jpg',
   newEpTimes: '&#128344; TODAY',
@@ -188,7 +188,7 @@ export const exclusivesAnimeInfos = [{
 },
 {
   id: 'C0000-0000-0004',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'LOVE FLOPS',
   image: 'css/ExclusivePosters/LFP_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; TODAY',
@@ -196,7 +196,7 @@ export const exclusivesAnimeInfos = [{
 },
 {
   id: 'C0000-0000-0005',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'PHANTOM OF THE IDOL',
   image: 'css/ExclusivePosters/PHI_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; TODAY',
@@ -204,7 +204,7 @@ export const exclusivesAnimeInfos = [{
 },
 {
   id: 'C0000-0001-0001',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'THE DREAMING BOY IS A REALIST',
   image: 'css/ExclusivePosters/DBR_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -212,7 +212,7 @@ export const exclusivesAnimeInfos = [{
 },
 {
   id: 'C0000-0001-0002',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'GUNBUSTER THE MOVIE',
   image: 'css/ExclusivePosters/GNB_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -220,7 +220,7 @@ export const exclusivesAnimeInfos = [{
 },
 {
   id: 'C0000-0001-0003',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'THE DEMON SWORD MASTER OF EXCLAIBUR ACADEMY',
   image: 'css/SimulcastPosters/DEA_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -228,7 +228,7 @@ export const exclusivesAnimeInfos = [{
 },
 {
   id: 'C0000-0001-0004',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'THE VEXATIONS OF A SHUT-IN VAMPIRE PRINCESS',
   image: 'css/SimulcastPosters/VVP_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -236,7 +236,7 @@ export const exclusivesAnimeInfos = [{
 },
 {
   id: 'C0000-0001-0005',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'RAGNA CRIMSON',
   image: 'css/SimulcastPosters/RCR_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -246,7 +246,7 @@ export const exclusivesAnimeInfos = [{
 
 export const IntroToHidiveAnimeInfos = [{
   id: 'D0000-0000-0001',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'REAL GIRL',
   image: 'css/IntroToHidivePosters/real-girl_RLG_01_MASTER_300x169_01.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -254,7 +254,7 @@ export const IntroToHidiveAnimeInfos = [{
 },
 {
   id: 'D0000-0000-0002',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'LEGEND OF THE GALACTIC HEROES',
   image: 'css/IntroToHidivePosters/LegendGalacticHeroes_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -262,7 +262,7 @@ export const IntroToHidiveAnimeInfos = [{
 },
 {
   id: 'D0000-0000-0003',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'BLOOM INTO YOU',
   image: 'css/IntroToHidivePosters/BIB_01_MASTER_300x169_01.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -270,7 +270,7 @@ export const IntroToHidiveAnimeInfos = [{
 },
 {
   id: 'D0000-0000-0004',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'GATE',
   image: 'css/IntroToHidivePosters/GTE_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -278,7 +278,7 @@ export const IntroToHidiveAnimeInfos = [{
 },
 {
   id: 'D0000-0000-0005',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'IS IT WRONG TO PICK UP GIRLS IN A DUNGEON',
   image: 'css/IntroToHidivePosters/WPG_OVA_MASTER_300x169_v02.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -286,7 +286,7 @@ export const IntroToHidiveAnimeInfos = [{
 },
 {
   id: 'D0000-0001-0001',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'MADE IN ABYSS',
   image: 'css/IntroToHidivePosters/made-in-abyss_MIA_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -294,7 +294,7 @@ export const IntroToHidiveAnimeInfos = [{
 },
 {
   id: 'D0000-0001-0002',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'FOOD WARS!',
   image: 'css/IntroToHidivePosters/FDW_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -302,7 +302,7 @@ export const IntroToHidiveAnimeInfos = [{
 },
 {
   id: 'D0000-0001-0003',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'MY GIRLFRIEND IS A SHOBITCH',
   image: 'css/IntroToHidivePosters/FVB_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -310,7 +310,7 @@ export const IntroToHidiveAnimeInfos = [{
 },
 {
   id: 'D0000-0001-0004',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: `MONTHLY GILRS' NOZAKI-KUN`,
   image: 'css/IntroToHidivePosters/MGN_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -318,7 +318,7 @@ export const IntroToHidiveAnimeInfos = [{
 },
 {
   id: 'D0000-0001-0005',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'DOMESTIC GIRLFRIEND',
   image: 'css/IntroToHidivePosters/DGF_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -328,7 +328,7 @@ export const IntroToHidiveAnimeInfos = [{
 
 export const TrendingNowAnimeInfos = [{
   id: 'E0000-0000-0001',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'THE EMINENCE IN SHADOW',
   image: 'css/SimulcastPosters/EIS_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -336,7 +336,7 @@ export const TrendingNowAnimeInfos = [{
 },
 {
   id: 'E0000-0000-0002',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'THE SEVEN HEAVENLY VIRTUES',
   image: 'css/TrendingnowPosters/SHV_01_MASTER_300x169_01.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -344,7 +344,7 @@ export const TrendingNowAnimeInfos = [{
 },
 {
   id: 'E0000-0000-0003',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'RE:CYCLE OF THE PENGUINDRUM MOVIE',
   image: 'css/TrendingnowPosters/PGD_MOVIE2_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -352,7 +352,7 @@ export const TrendingNowAnimeInfos = [{
 },
 {
   id: 'E0000-0000-0004',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'HELCK',
   image: 'css/SimulcastPosters/HLK_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -360,7 +360,7 @@ export const TrendingNowAnimeInfos = [{
 },
 {
   id: 'E0000-0000-0005',
-  hoverID: 'asdf',
+  hoverID: 'Displaying',
   name: 'THE DEMON SWORD MASTER OF EXCLAIBUR ACADEMY',
   image: 'css/SimulcastPosters/DEA_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -368,7 +368,7 @@ export const TrendingNowAnimeInfos = [{
 },
 {
   id: 'E0000-0001-0001',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'MANAGEMENT OF A NOVICE ALCHEMIST',
   image: 'css/TrendingnowPosters/MNA_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -376,7 +376,7 @@ export const TrendingNowAnimeInfos = [{
 },
 {
   id: 'E0000-0001-0002',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'ENDO AND KOBAYASHI, LIVE! THE LATEST ON TSUNDERE',
   image: 'css/TrendingnowPosters/EKL_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -384,7 +384,7 @@ export const TrendingNowAnimeInfos = [{
 },
 {
   id: 'E0000-0001-0003',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'LEVEL 1 DEMON LORD & ONE ROOM HERO',
   image: 'css/TrendingnowPosters/ORH_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -392,7 +392,7 @@ export const TrendingNowAnimeInfos = [{
 },
 {
   id: 'E0000-0001-0004',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'GATE',
   image: 'css/IntroToHidivePosters/GTE_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
@@ -400,7 +400,7 @@ export const TrendingNowAnimeInfos = [{
 },
 {
   id: 'E0000-0001-0005',
-  hoverID: 'asdfg',
+  hoverID: 'NotDisplaying',
   name: 'PHANTOM OF THE IDOL',
   image: 'css/ExclusivePosters/PHI_01_MASTER_300x169.jpg',
   newEpTimes: '&#128344; FINISHED',
