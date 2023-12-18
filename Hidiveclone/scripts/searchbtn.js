48b63b7e9da745c542286716c647a08d3376585d
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2023-12-18 01:53:45 -0500

    changed css of search button and mad trail disapear

--- Hidiveclone/scripts/searchbtn.js
+++ Hidiveclone/scripts/searchbtn.js
@@ -2,7 +2,14 @@
 let b = () => {
   document.getElementById('search-icon-div').innerHTML = `
   <input id="searchbar-after" type="text" placeholder="search">
-  <button id="Search-icon-btn-after" onclick="a()"><img class="Search-icon" src="css/images/icons8-search-48 (1).png"></button>  `
+  <button id="Search-icon-btn-after" onclick="a()"><img class="Search-icon-after" src="css/images/icons8-search-48 (1).png"></button>  `
+  document.getElementById('Trial-signup-div').innerHTML = `
+  <a href="https://www.hidive.com/dashboard">
+    <button class="Trial-signup-btn-after">
+      START 7-DAY FREE TRIAL
+    </button>
+  </a>
+  `
 }
 
 
@@ -10,4 +17,11 @@ let a = () => {
   document.getElementById('search-icon-div').innerHTML = `
   <input id="searchbar" type="text" placeholder="search">
   <button id="Search-icon-btn" onclick="b()"><img class="Search-icon" src="css/images/icons8-search-48 (1).png"></button>`
+  document.getElementById('Trial-signup-div').innerHTML = `
+  <a href="https://www.hidive.com/dashboard">
+    <button class="Trial-signup-btn">
+      START 7-DAY FREE TRIAL
+    </button>
+  </a>
+  `
 }
