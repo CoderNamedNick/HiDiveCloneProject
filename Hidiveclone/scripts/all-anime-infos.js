3fb5c111e460302a44a9cf13e39566eae6975cd4
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2023-12-19 22:37:43 -0500

    Create all-anime-infos.js

---
+++ Hidiveclone/scripts/all-anime-infos.js
@@ -0,0 +1,144 @@
+let AllAnimes = [{
+  id: 'A0000-0000-0001',
+  name: 'BLADE OF THE IMMORTAL',
+  image: 'css/animePosters/BOI_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0002',
+  name: 'SHIROBAKO',
+  image: 'css/animePosters/SHB_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0003',
+  name: `KINO'S JOURNEY`,
+  image: 'css/animePosters/KJ_Master_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0004',
+  name: 'THE DEMON SWORD MASTER OF EXCLAIBUR ACADEMY',
+  image: 'css/animePosters/DEA_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0005',
+  name: 'TO LOVERU DARKNESS 2',
+  image: 'css/animePosters/TLD_02_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0006',
+  name: 'HIMOUTO! UMARU-CHAN',
+  image: 'css/animePosters/HUC_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0007',
+  name: 'KIDS ON THE SLOPE',
+  image: 'css/animePosters/KOS_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0008',
+  name: 'MY LOVE STORY!!',
+  image: 'css/animePosters/MLS_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0009',
+  name: 'LOVE, CHUNIBYO & OTHER DELUSIONS! HEART THROB-OVA',
+  image: 'css/animePosters/CDK_02_OVA_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0010',
+  name: 'BEYOND THE BOUNDARY-OVA',
+  image: 'css/animePosters/BTB_OVA_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0011',
+  name: 'THE VEXATIONS OF A SHUT-IN VAMPIRE PRINCESS',
+  image: 'css/SimulcastPosters/VVP_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0012',
+  name: 'RAGNA CRIMSON',
+  image: 'css/SimulcastPosters/RCR_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0013',
+  name: 'DARK GATHERING',
+  image: 'css/SimulcastPosters/DKG_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0014',
+  name: 'HELCK',
+  image: 'css/SimulcastPosters/HLK_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0015',
+  name: 'THE EMINENCE IN SHADOW',
+  image: 'css/SimulcastPosters/EIS_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0016',
+  name: 'CARDFIGHT!! VANGAURD',
+  image: 'css/SimulcastPosters/VWD_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0017',
+  name: 'SPY CLASSROOM',
+  image: 'css/SimulcastPosters/SCL_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0018',
+  name: 'THE MOST HERETICAL LAST BOSS QUEEN',
+  image: 'css/SimulcastPosters/HLB_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'A0000-0000-0019',
+  name: 'THE DREAMING BOY IS A REALIST',
+  image: 'css/SimulcastPosters/DBR_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'C0000-0000-0002',
+  name: 'LOVE, CHUNIBYO & OTHER DELUSIONS!-OVA',
+  image: 'css/ExclusivePosters/CDK_01_OVA_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'C0000-0000-0003',
+  name: 'LOVE, CHUNIBYO & OTHER DELUSIONS! HEART THROB-OVA',
+  image: 'css/ExclusivePosters/CDK_02_OVA_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'C0000-0000-0004',
+  name: 'LOVE FLOPS',
+  image: 'css/ExclusivePosters/LFP_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'C0000-0000-0005',
+  name: 'PHANTOM OF THE IDOL',
+  image: 'css/ExclusivePosters/PHI_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+},
+{
+  id: 'C0000-0001-0002',
+  name: 'GUNBUSTER THE MOVIE',
+  image: 'css/ExclusivePosters/GNB_01_MASTER_300x169.jpg',
+  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+}]
