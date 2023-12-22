9c060ee473903c3defd6c3659efa7c4903ac94a0
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2023-12-21 20:18:34 -0500

    Update hello.js

--- Hidiveclone/scripts/hello.js
+++ Hidiveclone/scripts/hello.js
@@ -3,6 +3,7 @@
 const retrievedValue = localStorage.getItem('myVariable');
 
 
-//document.getElementById('search-result-div').innerHTML = `
-//  <p>${retrievedValue}</p>
-//`
+document.getElementById('search-result-div').innerHTML = `
+<input class="search-searchpage" type="text" value="${retrievedValue}">
+<button class="Search-icon-searchpage-btn" ><img class="Search-icon-searchpage" src="css/images/icons8-search-30.png"></button>
+`
