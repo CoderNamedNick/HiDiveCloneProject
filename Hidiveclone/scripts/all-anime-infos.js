1cfeb78d4b2570fd5f8afb1637e3e45d0c9d9cf6
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2023-12-22 20:09:06 -0500

    Update all-anime-infos.js

--- Hidiveclone/scripts/all-anime-infos.js
+++ Hidiveclone/scripts/all-anime-infos.js
@@ -8,115 +8,115 @@ export let AllAnimes = [{
   id: 'A0000-0000-0002',
   name: 'SHIROBAKO',
   image: 'css/animePosters/SHB_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: [' Comedy', 'Slice of Life']
 },
 {
   id: 'A0000-0000-0003',
   name: `KINO'S JOURNEY`,
   image: 'css/animePosters/KJ_Master_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action', 'Adventure', 'Science Fiction']
 },
 {
   id: 'A0000-0000-0004',
   name: 'THE DEMON SWORD MASTER OF EXCLAIBUR ACADEMY',
   image: 'css/animePosters/DEA_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action', 'fantasy', 'Adventure',]
 },
 {
   id: 'A0000-0000-0005',
   name: 'TO LOVERU DARKNESS 2',
   image: 'css/animePosters/TLD_02_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: [' Romance', 'Mature', 'Science Fiction', 'Action and Adventure']
 },
 {
   id: 'A0000-0000-0006',
   name: 'HIMOUTO! UMARU-CHAN',
   image: 'css/animePosters/HUC_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Family and Kids', 'Comedy', 'Slice of Life']
 },
 {
   id: 'A0000-0000-0007',
   name: 'KIDS ON THE SLOPE',
   image: 'css/animePosters/KOS_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['romance', 'Slice of Life', 'Drama']
 },
 {
   id: 'A0000-0000-0008',
   name: 'MY LOVE STORY!!',
   image: 'css/animePosters/MLS_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Romance', 'Comedy', 'Slice of Life']
 },
 {
   id: 'A0000-0000-0009',
   name: 'LOVE, CHUNIBYO & OTHER DELUSIONS! HEART THROB-OVA',
   image: 'css/animePosters/CDK_02_OVA_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Romance', 'Comedy', 'Slice of Life','Drama']
 },
 {
   id: 'A0000-0000-0010',
   name: 'BEYOND THE BOUNDARY-OVA',
   image: 'css/animePosters/BTB_OVA_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Drama', 'Fantasy', 'Supernatural', 'Action', 'Adventure']
 },
 {
   id: 'A0000-0000-0011',
   name: 'THE VEXATIONS OF A SHUT-IN VAMPIRE PRINCESS',
   image: 'css/SimulcastPosters/VVP_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Comedy', 'fantasy']
 },
 {
   id: 'A0000-0000-0012',
   name: 'RAGNA CRIMSON',
   image: 'css/SimulcastPosters/RCR_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action', 'fantasy','Supernatural']
 },
 {
   id: 'A0000-0000-0013',
   name: 'DARK GATHERING',
   image: 'css/SimulcastPosters/DKG_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Mature', 'Horror', 'Supernatural']
 },
 {
   id: 'A0000-0000-0014',
   name: 'HELCK',
   image: 'css/SimulcastPosters/HLK_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action', 'fantasy', ' Comedy']
 },
 {
   id: 'A0000-0000-0015',
   name: 'THE EMINENCE IN SHADOW',
   image: 'css/SimulcastPosters/EIS_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action', 'fantasy','Supernatural', 'Adventure',]
 },
 {
   id: 'A0000-0000-0016',
   name: 'CARDFIGHT!! VANGAURD',
   image: 'css/SimulcastPosters/VWD_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action', 'Adventure']
 },
 {
   id: 'A0000-0000-0017',
   name: 'SPY CLASSROOM',
   image: 'css/SimulcastPosters/SCL_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action', 'Shounen', ' Drama']
 },
 {
   id: 'A0000-0000-0018',
   name: 'THE MOST HERETICAL LAST BOSS QUEEN',
   image: 'css/SimulcastPosters/HLB_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Romance', 'fantasy']
 },
 {
   id: 'A0000-0000-0019',
   name: 'THE DREAMING BOY IS A REALIST',
   image: 'css/SimulcastPosters/DBR_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Romance',' Drama','Slice of Life']
 },
 {
   id: 'C0000-0000-0002',
   name: 'LOVE, CHUNIBYO & OTHER DELUSIONS!-OVA',
   image: 'css/ExclusivePosters/CDK_01_OVA_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre:  ['Romance', 'Comedy', 'Slice of Life','Drama']
 },
 {
   id: 'C0000-0000-0003',
@@ -128,119 +128,109 @@ export let AllAnimes = [{
   id: 'C0000-0000-0004',
   name: 'LOVE FLOPS',
   image: 'css/ExclusivePosters/LFP_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Scince Fiction', 'Romance', ' Comedy']
 },
 {
   id: 'C0000-0000-0005',
   name: 'PHANTOM OF THE IDOL',
   image: 'css/ExclusivePosters/PHI_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Comedy', 'Supernatural']
 },
 {
   id: 'C0000-0001-0002',
   name: 'GUNBUSTER THE MOVIE',
   image: 'css/ExclusivePosters/GNB_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Scince Fiction', 'action', 'Adventure', 'Mecha']
 },
 {
   id: 'D0000-0000-0001',
   name: 'REAL GIRL',
   image: 'css/IntroToHidivePosters/real-girl_RLG_01_MASTER_300x169_01.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Shoujo', 'Romance', 'Comedy']
 },
 {
   id: 'D0000-0000-0002',
   name: 'LEGEND OF THE GALACTIC HEROES',
   image: 'css/IntroToHidivePosters/LegendGalacticHeroes_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action', 'Mature', 'Science Fiction', 'Drama']
 },
 {
   id: 'D0000-0000-0003',
   name: 'BLOOM INTO YOU',
   image: 'css/IntroToHidivePosters/BIB_01_MASTER_300x169_01.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Romace',' Drama', 'Slice of Life']
 },
 {
   id: 'D0000-0000-0004',
   name: 'GATE',
   image: 'css/IntroToHidivePosters/GTE_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action', 'fantasy', 'Supernatural']
 },
 {
   id: 'D0000-0000-0005',
   name: 'IS IT WRONG TO PICK UP GIRLS IN A DUNGEON',
   image: 'css/IntroToHidivePosters/WPG_OVA_MASTER_300x169_v02.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action', 'fantasy', 'Romance', 'Supernatural', 'Seinen',]
 },
 {
   id: 'D0000-0001-0001',
   name: 'MADE IN ABYSS',
   image: 'css/IntroToHidivePosters/made-in-abyss_MIA_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action', 'Science Fiction', 'Mystery', 'Thriller', 'Fantasy',]
 },
 {
   id: 'D0000-0001-0002',
   name: 'FOOD WARS!',
   image: 'css/IntroToHidivePosters/FDW_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Comedy', 'Slice of Life']
 },
 {
   id: 'D0000-0001-0003',
   name: 'MY GIRLFRIEND IS A SHOBITCH',
   image: 'css/IntroToHidivePosters/FVB_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Slice of Life', 'Comedy', 'Romance']
 },
 {
   id: 'D0000-0001-0004',
   name: `MONTHLY GILRS' NOZAKI-KUN`,
   image: 'css/IntroToHidivePosters/MGN_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Comedy', 'Slice of Life', 'Romance',]
 },
 {
   id: 'D0000-0001-0005',
   name: 'DOMESTIC GIRLFRIEND',
   image: 'css/IntroToHidivePosters/DGF_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Romance',' Drama']
 },
 {
   id: 'E0000-0000-0002',
-  hoverID: 'Displaying',
   name: 'THE SEVEN HEAVENLY VIRTUES',
   image: 'css/TrendingnowPosters/SHV_01_MASTER_300x169_01.jpg',
-  newEpTimes: '&#128344; FINISHED',
-  opacity: 1,
+  genre: ['Mature','Action','Fantasy']
 },
 {
   id: 'E0000-0000-0003',
-  hoverID: 'Displaying',
   name: 'RE:CYCLE OF THE PENGUINDRUM MOVIE',
   image: 'css/TrendingnowPosters/PGD_MOVIE2_MASTER_300x169.jpg',
-  newEpTimes: '&#128344; FINISHED',
-  opacity: 1,
+  genre: ['Supernatural',' Drama','Mystery','Thriller']
 },
 {
   id: 'E0000-0001-0001',
-  hoverID: 'NotDisplaying',
   name: 'MANAGEMENT OF A NOVICE ALCHEMIST',
   image: 'css/TrendingnowPosters/MNA_01_MASTER_300x169.jpg',
-  newEpTimes: '&#128344; FINISHED',
-  opacity: 0,
+  genre: ['Fantasy','Adventure','Comedy','Slice of Life','Fantasy']
 },
 {
   id: 'E0000-0001-0002',
-  hoverID: 'NotDisplaying',
   name: 'ENDO AND KOBAYASHI, LIVE! THE LATEST ON TSUNDERE',
   image: 'css/TrendingnowPosters/EKL_01_MASTER_300x169.jpg',
-  newEpTimes: '&#128344; FINISHED',
-  opacity: 0,
+  genre: ['Romance','Comedy','Shoujo']
 },
 {
   id: 'E0000-0001-0003',
-  hoverID: 'NotDisplaying',
   name: 'LEVEL 1 DEMON LORD & ONE ROOM HERO',
   image: 'css/TrendingnowPosters/ORH_01_MASTER_300x169.jpg',
-  newEpTimes: '&#128344; FINISHED',
-  opacity: 0,
+  genre: ['Comedy','Fantasy','Seinen']
 }];
 
 
