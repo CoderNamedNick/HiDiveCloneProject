c08cbac7d55f650c6ee7253e82c51a0405a7986b
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2023-12-22 20:46:50 -0500

    Update all-anime-infos.js

--- Hidiveclone/scripts/all-anime-infos.js
+++ Hidiveclone/scripts/all-anime-infos.js
@@ -2,235 +2,274 @@ export let AllAnimes = [{
   id: 'A0000-0000-0001',
   name: 'BLADE OF THE IMMORTAL',
   image: 'css/animePosters/BOI_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['Action ', 'Fantasy ', 'Drama ', 'Supernatural ', 'Seinen',],
+  paragraph: 'When a young girl named Rin asks Manji, the Slayer of Hundreds, to help her take revenge on the group of master swordsmen who murdered her family, Manji sees an opportunity to lift his curse of eternal life and put an end to his infinite suffering.'
 },
 {
   id: 'A0000-0000-0002',
   name: 'SHIROBAKO',
   image: 'css/animePosters/SHB_MASTER_300x169.jpg',
-  genre: [' Comedy', 'Slice of Life']
+  genre: ['Comedy ', 'Slice of Life'],
+  paragraph: 'Aoi, Ema, Shizuka, Misa, and Midori make a promise over donuts to follow their dreams and work in the anime industry. Since then, day after day, the five girls have spent all of their time trying to fulfil their promise.'
 },
 {
   id: 'A0000-0000-0003',
   name: `KINO'S JOURNEY`,
   image: 'css/animePosters/KJ_Master_300x169.jpg',
-  genre: ['action', 'Adventure', 'Science Fiction']
+  genre: ['Action ', 'Adventure ', 'Science Fiction'],
+  paragraph: 'Accompanied by a talking motorcycle, Kino travels the globe and witnesses both the beauty and the brutality of the world and its people.'
 },
 {
   id: 'A0000-0000-0004',
   name: 'THE DEMON SWORD MASTER OF EXCLAIBUR ACADEMY',
   image: 'css/animePosters/DEA_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', 'Adventure',]
+  genre: ['Action ', 'Fantasy ', 'Adventure'],
+  paragraph: 'An undead sorcerer and powerful Dark Lord awakens as a ten-year-old boy a thousand years after entering magical stasis during a climactic battle. Now he has to adapt to a strange new era and deal with the twisted monsters called Voids.'
 },
 {
   id: 'A0000-0000-0005',
   name: 'TO LOVERU DARKNESS 2',
   image: 'css/animePosters/TLD_02_MASTER_300x169.jpg',
-  genre: [' Romance', 'Mature', 'Science Fiction', 'Action and Adventure']
+  genre: ['Romance ', 'Mature ', 'Science Fiction ', 'Action and Adventure'],
+  paragraph: 'Rito and the princesses of Deviluke are back again! When the arrival of a new girl threatens one of their own, it’s up to Rito and the girls to fend off the unwelcome advances of a mysterious adversary, or else they might lose someone for good.'
 },
 {
   id: 'A0000-0000-0006',
   name: 'HIMOUTO! UMARU-CHAN',
   image: 'css/animePosters/HUC_MASTER_300x169.jpg',
-  genre: ['Family and Kids', 'Comedy', 'Slice of Life']
+  genre: ['Family and Kids ', 'Comedy ', 'Slice of Life'],
+  paragraph: 'In public, Umaru is the perfect vision of a beautiful and smart high school girl. At home, she morphs into the ultimate slob with the ability to game all night and binge on junk food and cola. Can her big brother, Taihei, change her?'
 },
 {
   id: 'A0000-0000-0007',
   name: 'KIDS ON THE SLOPE',
   image: 'css/animePosters/KOS_MASTER_300x169.jpg',
-  genre: ['romance', 'Slice of Life', 'Drama']
+  genre: ['Romance ', 'Slice of Life ', 'Drama'],
+  paragraph: 'Karou has moved from town to town his entire life, never able to live in one place long enough to consider it home. After his most recent move he expects more of the same until he meets Sentaro who introduces him to his newest passion, jazz.'
 },
 {
   id: 'A0000-0000-0008',
   name: 'MY LOVE STORY!!',
   image: 'css/animePosters/MLS_MASTER_300x169.jpg',
-  genre: ['Romance', 'Comedy', 'Slice of Life']
+  genre: ['Romance ', 'Comedy ', 'Slice of Life'],
+  paragraph: 'Takeo Goda is a big guy with a big heart, but girls like his attractive best friend more. Yet when Takeo helps a girl who was being harassed on the train, his love life may take a turn for the better!'
 },
 {
   id: 'A0000-0000-0009',
   name: 'LOVE, CHUNIBYO & OTHER DELUSIONS! HEART THROB-OVA',
   image: 'css/animePosters/CDK_02_OVA_MASTER_300x169.jpg',
-  genre: ['Romance', 'Comedy', 'Slice of Life','Drama']
+  genre: ['Romance ', 'Comedy ', 'Slice of Life ','Drama'],
+  paragraph: 'Problems arise for Yuta and Rikka after Makoto gives Yuta a USB containing pictures of an idol that he had a crush on in middle school. After Rikka asks Yuta to return the stick, he secretly keeps it and Rikka finds after sneaking into his room.'
 },
 {
   id: 'A0000-0000-0010',
   name: 'BEYOND THE BOUNDARY-OVA',
   image: 'css/animePosters/BTB_OVA_MASTER_300x169.jpg',
-  genre: ['Drama', 'Fantasy', 'Supernatural', 'Action', 'Adventure']
+  genre: ['Drama ', 'Fantasy ', 'Supernatural ', 'Action ', 'Adventure'],
+  paragraph: 'Set two years before Akihito met Mirai, the Nase siblings, Hiroomi and Mitsuki, first encounter Akihito when they are sent to subdue him and bring back by any means necessary'
 },
 {
   id: 'A0000-0000-0011',
   name: 'THE VEXATIONS OF A SHUT-IN VAMPIRE PRINCESS',
   image: 'css/SimulcastPosters/VVP_01_MASTER_300x169.jpg',
-  genre: ['Comedy', 'fantasy']
+  genre: ['Comedy ', 'fantasy '],
+  paragraph: `After three years as a shut-in, a vampire known as Komari awakens to find she's been appointed as a Commander in the Mulnite Imperial Army! Unfortunately, her new unit consists of belligerent ruffians who revolt at the slightest hint of weakness.`
 },
 {
   id: 'A0000-0000-0012',
   name: 'RAGNA CRIMSON',
   image: 'css/SimulcastPosters/RCR_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy','Supernatural']
+  genre: ['action ', 'fantasy ','Supernatural'],
+  paragraph: `Ragna teams up with the enigmatic Crimson to stand against the dragons menacing the world. Although Crimson’s motivations are mysterious, his goal and Ragna’s perfectly align, and together they’ll vanquish the dragons once and for all.`
 },
 {
   id: 'A0000-0000-0013',
   name: 'DARK GATHERING',
   image: 'css/SimulcastPosters/DKG_01_MASTER_300x169.jpg',
-  genre: ['Mature', 'Horror', 'Supernatural']
+  genre: ['Mature ', 'Horror ', 'Supernatural'],
+  paragraph: 'Keitaro takes a part-time job tutoring the child prodigy Yayoi. Little does he know she’s a gifted medium hell-bent on finding the spirit behind her mother’s vanishing and she’s gravely determined to drag Keitaro into a terrifying supernatural world.'
 },
 {
   id: 'A0000-0000-0014',
   name: 'HELCK',
   image: 'css/SimulcastPosters/HLK_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Comedy']
+  genre: ['action ', 'fantasy ', ' Comedy'],
+  paragraph: `Why would a human want to kill all humans? What secrets is he hiding? A world-shaking plot is afoot, and human hero Helck may just be the demons’ last hope..`
 },
 {
   id: 'A0000-0000-0015',
   name: 'THE EMINENCE IN SHADOW',
   image: 'css/SimulcastPosters/EIS_01_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy','Supernatural', 'Adventure',]
+  genre: ['action ', 'fantasy ','Supernatural ', 'Adventure'],
+  paragraph: `When Cid is isekai’d to another world, he creates an underground organization to live out a fight against a made-up cult. Little does he know the cult is real, and they’re not happy his power fantasy just impeded their plans!`
 },
 {
   id: 'A0000-0000-0016',
   name: 'CARDFIGHT!! VANGAURD',
   image: 'css/SimulcastPosters/VWD_01_MASTER_300x169.jpg',
-  genre: ['action', 'Adventure']
+  genre: ['action ', 'Adventure'],
+  paragraph: '15-year-old Yu-yu discovers a nocturnal amusement park where the members of Team Blackout play Vanguard each night. Yu-yu soon finds himself drawn into epic battles and thrilling action as he and Team Blackout fight to maintain their territory.'
 },
 {
   id: 'A0000-0000-0017',
   name: 'SPY CLASSROOM',
   image: 'css/SimulcastPosters/SCL_01_MASTER_300x169.jpg',
-  genre: ['action', 'Shounen', ' Drama']
+  genre: ['action ', 'Shounen ', ' Drama'],
+  paragraph: 'Spy-in-training Lily is desperate to score a passing grade. Eager to prove herself, she leaps at the chance to join the mysterious team Lamplight and beat the Impossible Mission. Too bad the task ahead is even more harrowing than she imagined…'
 },
 {
   id: 'A0000-0000-0018',
   name: 'THE MOST HERETICAL LAST BOSS QUEEN',
   image: 'css/SimulcastPosters/HLB_01_MASTER_300x169.jpg',
-  genre: ['Romance', 'fantasy']
+  genre: ['Romance ', 'fantasy'],
+  paragraph: `Our protagonist was just reincarnated as Princess Pride Royal Ivy, the last boss and ultimate villain of her favorite otome game. But evil just isn’t her style, so she’ll use Pride’s powerful abilities to sow peace and love instead of discord!`
 },
 {
   id: 'A0000-0000-0019',
   name: 'THE DREAMING BOY IS A REALIST',
   image: 'css/SimulcastPosters/DBR_01_MASTER_300x169.jpg',
-  genre: ['Romance',' Drama','Slice of Life']
+  genre: ['Romance ','Drama ','Slice of Life'],
+  paragraph: `Did Wataru’s intentions slip by Aika because she was getting impatient?! This is the start of a romcom revolving around two people who just can't get their feelings across and both think their love is unrequited!`
 },
 {
   id: 'C0000-0000-0002',
   name: 'LOVE, CHUNIBYO & OTHER DELUSIONS!-OVA',
   image: 'css/ExclusivePosters/CDK_01_OVA_MASTER_300x169.jpg',
-  genre:  ['Romance', 'Comedy', 'Slice of Life','Drama']
+  genre:  ['Romance ', 'Comedy ', 'Slice of Life ','Drama'],
+  paragraph: `With Christmas fast approaching, Rikka and Nibutani plan a Christmas party at Sanae's Mansion after Makoto's suggestion. Chaos ensues at the party as Rikka and Yuta continue exploring their burgeoning relationship.`
 },
 {
   id: 'C0000-0000-0003',
   name: 'LOVE, CHUNIBYO & OTHER DELUSIONS! HEART THROB-OVA',
   image: 'css/ExclusivePosters/CDK_02_OVA_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', ' Drama', 'Supernatural', 'Seinen',]
+  genre: ['action ', 'fantasy ', 'Drama ', 'Supernatural ', 'Seinen'],
+  paragraph: `Problems arise for Yuta and Rikka after Makoto gives Yuta a USB containing pictures of an idol that he had a crush on in middle school. After Rikka asks Yuta to return the stick, he secretly keeps it and Rikka finds after sneaking into his room.`
 },
 {
   id: 'C0000-0000-0004',
   name: 'LOVE FLOPS',
   image: 'css/ExclusivePosters/LFP_01_MASTER_300x169.jpg',
-  genre: ['Scince Fiction', 'Romance', ' Comedy']
+  genre: ['Scince Fiction ', 'Romance ', ' Comedy'],
+  paragraph: `Destiny draws five beautiful people into Asahi’s path, and he soon receives not one, not two, but five love confessions. Asahi must follow his heart to find which of the five is his perfect match, else his love fortunes may end in an epic flop!`
 },
 {
   id: 'C0000-0000-0005',
   name: 'PHANTOM OF THE IDOL',
   image: 'css/ExclusivePosters/PHI_01_MASTER_300x169.jpg',
-  genre: ['Comedy', 'Supernatural']
+  genre: ['Comedy ', 'Supernatural'],
+  paragraph: `Yuya is part of the idol duo ZINGS, but his laziness and disdain for fans puts him in danger of getting blacklisted. That is, until he meets Asahi, the ghost of a former idol who’s eager to stage her own comeback by possessing Yuya's willing body!`
 },
 {
   id: 'C0000-0001-0002',
   name: 'GUNBUSTER THE MOVIE',
   image: 'css/ExclusivePosters/GNB_01_MASTER_300x169.jpg',
-  genre: ['Scince Fiction', 'action', 'Adventure', 'Mecha']
+  genre: ['Scince Fiction ', 'action ', 'Adventure ', 'Mecha'],
+  paragraph: `There are monsters between the stars, alien nightmares seeking to destroy the human race. Noriko Takaya is training to pilot a giant robot to fight them, but, despite the reality of their enemies, Noriko and the other pilots’ real adversary is time.`
 },
 {
   id: 'D0000-0000-0001',
   name: 'REAL GIRL',
   image: 'css/IntroToHidivePosters/real-girl_RLG_01_MASTER_300x169_01.jpg',
-  genre: ['Shoujo', 'Romance', 'Comedy']
+  genre: ['Shoujo ', 'Romance ', 'Comedy'],
+  paragraph: `The reclusive Hikari Tsutsui has an aversion to real-world girls. That is, until he meets the popular Iroha Igarashi. The couple quickly develops a close bond neither expected, one that challenges their preconceived notions of who people truly are.`
 },
 {
   id: 'D0000-0000-0002',
   name: 'LEGEND OF THE GALACTIC HEROES',
   image: 'css/IntroToHidivePosters/LegendGalacticHeroes_MASTER_300x169.jpg',
-  genre: ['action', 'Mature', 'Science Fiction', 'Drama']
+  genre: ['action ', 'Mature ', 'Science Fiction ', 'Drama'],
+  paragraph: `War rages between the Galactic Empire and the Free Planets Alliance. With two new brilliant young leaders at the helm, anything can happen in this military space opera.`
 },
 {
   id: 'D0000-0000-0003',
   name: 'BLOOM INTO YOU',
   image: 'css/IntroToHidivePosters/BIB_01_MASTER_300x169_01.jpg',
-  genre: ['Romace',' Drama', 'Slice of Life']
+  genre: ['Romace ',' Drama ', 'Slice of Life'],
+  paragraph: `Yuu has always dreamt of receiving a love confession but feels nothing when a boy gives her one. Confused, she starts her first year of high school and meets Touko. Will Yuu’s heart finally skip a beat?`
 },
 {
   id: 'D0000-0000-0004',
   name: 'GATE',
   image: 'css/IntroToHidivePosters/GTE_MASTER_300x169.jpg',
-  genre: ['action', 'fantasy', 'Supernatural']
+  genre: ['Action ', 'Fantasy ', 'Supernatural '],
+  paragraph: `His failed attempt to save a doujinshi sale from an otherworldly attack turned Itami into an accidental hero. He’s promoted to second lieutenant of the Self-Defense Force and sent on a mission through the gate into another world.`
 },
 {
   id: 'D0000-0000-0005',
   name: 'IS IT WRONG TO PICK UP GIRLS IN A DUNGEON',
   image: 'css/IntroToHidivePosters/WPG_OVA_MASTER_300x169_v02.jpg',
-  genre: ['action', 'fantasy', 'Romance', 'Supernatural', 'Seinen',]
+  genre: ['action ', 'fantasy ', 'Romance ', 'Supernatural ', 'Seinen'],
+  paragraph: `A fateful encounter - that is what adventuring is all about! This comedy attempts to answer its title’s question as a young adventurer, blessed by the tiny Goddess Hestia, seeks to impress the perfect girl in a dungeon filled with mythical beasts.`
 },
 {
   id: 'D0000-0001-0001',
   name: 'MADE IN ABYSS',
   image: 'css/IntroToHidivePosters/made-in-abyss_MIA_MASTER_300x169.jpg',
-  genre: ['action', 'Science Fiction', 'Mystery', 'Thriller', 'Fantasy',]
+  genre: ['action ', 'Science Fiction ', 'Mystery ', 'Thriller ', 'Fantasy'],
+  paragraph: `Within the depths of the Abyss, a girl named Riko stumbles upon a robot who looks like a young boy. Riko and her new friend descend into uncharted territory to unlock its mysteries, but what lies in wait for them in the darkness?`
 },
 {
   id: 'D0000-0001-0002',
   name: 'FOOD WARS!',
   image: 'css/IntroToHidivePosters/FDW_MASTER_300x169.jpg',
-  genre: ['Comedy', 'Slice of Life']
+  genre: ['Comedy ', 'Slice of Life'],
+  paragraph: `Things are about to heat up for young Soma as he battles some of the world's best chefs to prove himself and refine his skills in the first season of Food Wars!`
 },
 {
   id: 'D0000-0001-0003',
   name: 'MY GIRLFRIEND IS A SHOBITCH',
   image: 'css/IntroToHidivePosters/FVB_01_MASTER_300x169.jpg',
-  genre: ['Slice of Life', 'Comedy', 'Romance']
+  genre: ['Slice of Life ', 'Comedy ', 'Romance'],
+  paragraph: `When Shinozaki confesses to the Akiho Kosaka, the last thing he expected was for her to say “yes.” In an effort to be a good girlfriend, Kosaka decides to study all manner of ways to keep Shinozaki sexually satisfied… in the theoretical sense.`
 },
 {
   id: 'D0000-0001-0004',
   name: `MONTHLY GILRS' NOZAKI-KUN`,
   image: 'css/IntroToHidivePosters/MGN_MASTER_300x169.jpg',
-  genre: ['Comedy', 'Slice of Life', 'Romance',]
+  genre: ['Comedy ', 'Slice of Life ', 'Romance'],
+  paragraph: `When Sakura's confession to her crush Nozaki goes awry, she instead finds herself working as his assistant for his side-job as a manga creator!`
 },
 {
   id: 'D0000-0001-0005',
   name: 'DOMESTIC GIRLFRIEND',
   image: 'css/IntroToHidivePosters/DGF_01_MASTER_300x169.jpg',
-  genre: ['Romance',' Drama']
+  genre: ['Romance ','Drama'],
+  paragraph: `Natsuo’s life suddenly becomes more complicated when his father comes home and announces he has remarried a woman with two daughters whom Natsuo has met before. They’re Hina, his teacher and unrequited crush, and Rui, a girl he previously slept with.`
 },
 {
   id: 'E0000-0000-0002',
   name: 'THE SEVEN HEAVENLY VIRTUES',
   image: 'css/TrendingnowPosters/SHV_01_MASTER_300x169_01.jpg',
-  genre: ['Mature','Action','Fantasy']
+  genre: ['Mature ','Action ','Fantasy'],
+  paragraph: `Heaven has been thrown into chaos, and the Seven Heavenly Virtues, are sent to Earth to find the “true messiah”. As these beauties from the sky try to race against time, they realize how far the Seven Mortal Sins have spread their plague.`
 },
 {
   id: 'E0000-0000-0003',
   name: 'RE:CYCLE OF THE PENGUINDRUM MOVIE',
   image: 'css/TrendingnowPosters/PGD_MOVIE2_MASTER_300x169.jpg',
-  genre: ['Supernatural',' Drama','Mystery','Thriller']
+  genre: ['Supernatural ','Drama ','Mystery ','Thriller'],
+  paragraph: `Is destiny a matter of chance or a matter of choice? To keep their terminally ill sister, Himari, alive, Kanba and Shouma must find the mysterious Penguindrum.`
 },
 {
   id: 'E0000-0001-0001',
   name: 'MANAGEMENT OF A NOVICE ALCHEMIST',
   image: 'css/TrendingnowPosters/MNA_01_MASTER_300x169.jpg',
-  genre: ['Fantasy','Adventure','Comedy','Slice of Life','Fantasy']
+  genre: ['Fantasy ','Adventure ','Comedy ','Slice of Life','Fantasy'],
+  paragraph: `Sarasa was gifted a house to use as an alchemy storefront, but it’s smackdab in the middle of nowhere! With customers to find and supplies to gather, Sarasa has her work cut out for her as she transmutes this bad location into a golden opportunity.`
 },
 {
   id: 'E0000-0001-0002',
   name: 'ENDO AND KOBAYASHI, LIVE! THE LATEST ON TSUNDERE',
   image: 'css/TrendingnowPosters/EKL_01_MASTER_300x169.jpg',
-  genre: ['Romance','Comedy','Shoujo']
+  genre: ['Romance ','Comedy ','Shoujo'],
+  paragraph: `Endo and Kobayashi love to bicker about their latest video game fixation. They think they're simply providing color commentary, but one of the game’s characters, Siegwald, can hear them talking about his fiancée, the villainess Lieselotte!`
 },
 {
   id: 'E0000-0001-0003',
   name: 'LEVEL 1 DEMON LORD & ONE ROOM HERO',
   image: 'css/TrendingnowPosters/ORH_01_MASTER_300x169.jpg',
-  genre: ['Comedy','Fantasy','Seinen']
+  genre: ['Comedy ','Fantasy ','Seinen'],
+  paragraph: `The Demon Lord seeks revenge on Max, the hero, after a humiliating defeat ten years ago! Unfortunately, Max now has no interest in an epic rematch after his humiliating fall from hero to zero.`
 }];
 
 
@@ -245,13 +284,13 @@ export let getsearch = (param, param2) => {
               <h2>${Anime.name}</h2>
             </div>
             <div class="searched-anime-Stats">
-              <p>SEASON 1 | 24 EPISODES | TV-PG</p>
+              <p>Genre: ${Anime.genre}</p>
             </div>
             <div class="searched-anime-paragraph">
-              <p>Despite Takeos big heart, girls dont want him. Hes used to it. But his luck may change when he meets Yamato!</p>
+              <p>${Anime.paragraph}</p>
             </div>
             <div class="searched-anime-watchnow">
-              <p><a>Show Info</a>  <a>Watch Now</a></p>
+              <p><a style="cursor: pointer;">  Show Info</a>  <a style="cursor: pointer;">  Watch Now</a></p>
             </div>
           </div>
       `
