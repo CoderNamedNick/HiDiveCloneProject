7d2e1e4015a2b343d56999e71e7a3d52e9decae8
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2023-12-24 01:41:26 -0500

    finished video player

--- Hidiveclone/scripts/animePlayer.js
+++ Hidiveclone/scripts/animePlayer.js
@@ -7,6 +7,29 @@ console.log(retrievedAnimeID)
 
 AllAnimes.forEach(Anime => {
   if (Anime.id === retrievedAnimeID) {
-    console.log('hellllooooooo')
+    document.getElementById('all-video-div').innerHTML = `
+    <div class="VIDEO">
+          Obviously due to copyright i cannot have the anime episode
+        </div>
+        <div class="video-description">
+          <div class="video-name">
+            <p>${Anime.name}</p>
+          </div>
+          <div>
+            <button class="video-buttons">add to que</button>
+            <button class="video-buttons">add favorite</button>
+          </div>
+          <hr class="hr1">
+          <div class="video-stats">
+            SEASON 1 EPISODE 1 | TV-PG | PREMIERE: 10/5/2018
+          </div>
+          <div class="episode-description">
+            <h1 class="video-title">First episode Title</h1>
+            <p>
+              ${Anime.paragraph}
+            </p>
+          </div>
+        </div>
+    `
   }
 });
